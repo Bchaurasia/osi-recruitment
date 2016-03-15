e28dfbad9a42cf72af76455daf4a14e785ce0bea
dthorat <dthorat@nisum.com>
2016-03-15 18:06:27 +0530

    106 : Fixing dependencies issue related to elastic search

--- src/main/java/com/nisum/employee/ref/controller/MongoConfig.java
+++ src/main/java/com/nisum/employee/ref/controller/MongoConfig.java
@@ -36,5 +36,10 @@ public class MongoConfig extends AbstractMongoConfiguration{
 	public Mongo mongo() throws Exception {
 		return new MongoClient(mongoClient,Integer.valueOf(mongoPort));
 	}
+	
+	@Override
+	protected String getMappingBasePackage() {
+		return "com.nisum.employee.ref.domain";
+	}
 
 }
