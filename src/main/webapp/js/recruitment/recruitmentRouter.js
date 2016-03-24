8c2b2a77a319f3d70c9f04ec25861f7c347d1486
sbhattacharya <sbhattacharya@nisum.com>
2016-03-24 15:10:36 +0530

    Defect_113 - [Somdev] Access denied for ROLE_REQUISITION_APPROVER and ROLE_REQUISITION_MANAGER fixed

--- src/main/webapp/js/recruitment/recruitmentRouter.js
+++ src/main/webapp/js/recruitment/recruitmentRouter.js
@@ -4,75 +4,75 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
 	.state('recruitment', {url:'/recruitment', views: {'': {templateUrl: 'views/recruitment/recruitment.html', controller: 'recruitmentCtrl'}},
     	resolve : {
     		permission: function(authorizationService,$route) {
-    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER","ROLE_ADMIN"]);
+    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER","ROLE_ADMIN","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
     		}
     	}
     })
     .state('recruitment.searchProfile', {url:'/searchProfile', views: {'': {templateUrl: 'views/recruitment/searchProfile.html', controller: 'searchProfileCtrl'}},
     	resolve : {
     		permission: function(authorizationService,$route) {
-    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER","ROLE_ADMIN","ROLE_USER"]);
+    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER","ROLE_ADMIN","ROLE_USER","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
     		}
     	}
     })
     .state('recruitment.createProfile', {url:'/createProfile', views: {'': {templateUrl: 'views/recruitment/createProfile.html', controller: 'createProfileCtrl'}},
     	resolve : {
         	permission: function(authorizationService,$route) {
-        		return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER","ROLE_USER"]);
+        		return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER","ROLE_USER","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
                }
         }
     })
     .state('recruitment.viewProfile', {url:'/viewProfile', views: {'': {templateUrl: 'views/recruitment/viewProfile.html', controller: 'editProfileCtrl'}},
     	resolve : {
         	permission: function(authorizationService,$route) {
-        		return authorizationService.permissionCheck(["ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER","ROLE_ADMIN","ROLE_USER"]);
+        		return authorizationService.permissionCheck(["ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER","ROLE_ADMIN","ROLE_USER","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
                }
         }
     })
     .state('recruitment.searchPosition', {url:'/searchPosition', views: {'': {templateUrl: 'views/recruitment/searchPosition.html', controller: 'searchPositionCtrl'}},
     	resolve : {
     		permission: function(authorizationService,$route) {
-    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_MANAGER","ROLE_ADMIN"]);
+    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_MANAGER","ROLE_ADMIN","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
             }
     	}
     })
     .state('recruitment.createPosition', {url:'/createPosition', views: {'': {templateUrl: 'views/recruitment/createPosition.html', controller: 'createPositionCtrl'}},
     	resolve : {
     		permission: function(authorizationService,$route) {
-    			return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_HR"]);
+    			return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_HR","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
             }
     	}
     })
     .state('recruitment.viewPosition', {url:'/viewPosition', views: {'': {templateUrl: 'views/recruitment/viewPosition.html', controller: 'editPositionCtrl'}},
     	resolve : {
     		permission: function(authorizationService,$route) {
-    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_MANAGER","ROLE_ADMIN"]);
+    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_MANAGER","ROLE_ADMIN","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
             }
     	}
     })
     .state('recruitment.interviewManagement', {url:'/interviewManagement', views: {'': {templateUrl: 'views/recruitment/searchInterview.html', controller: 'interviewManagementCtrl'}},
     	resolve : {
     		permission: function(authorizationService,$route) {
-    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER","ROLE_ADMIN"]);
+    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER","ROLE_ADMIN","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
     		}
     	}
     })
     .state('recruitment.interviewFeedback', {url:'/interviewFeedback', views: {'': {templateUrl: 'views/recruitment/interviewFeedback.html', controller: 'interviewFeedbackCtrl'}},
     	resolve : {
     		permission: function(authorizationService,$route) {
-    			return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER"]);
+    			return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
             }
     	}})
     .state('recruitment.scheduleInterview', {url:'/scheduleInterview', views: {'': {templateUrl: 'views/recruitment/scheduleInterview.html', controller: 'scheduleInterviewCtrl'}},
     	resolve : {
     		permission: function(authorizationService,$route) {
-    			return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER"]);
+    			return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
             }
     	}})
     .state('recruitment.showInterview', {url:'/showInterview', views: {'': {templateUrl: 'views/recruitment/showInterview.html', controller: 'showInterviewCtrl'}},
     	resolve : {
     		permission: function(authorizationService,$route) {
-    			return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER"]);
+    			return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_HR","ROLE_INTERVIEWER","ROLE_MANAGER","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
             }
     	}})
     	
@@ -81,29 +81,29 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
     	.state('recruitment.searchRequisition', {url:'/searchRequisition', views: {'': {templateUrl: 'views/recruitment/searchRequisition.html', controller: 'searchRequisitionCtrl'}},
     	resolve : {
     		permission: function(authorizationService,$route) {
-    			return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_HR","ROLE_MANAGER","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVAL"]);
+    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_MANAGER","ROLE_ADMIN","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
             }
     	}
-	    })
-	    .state('recruitment.createRequisition', {url:'/createRequisition', views: {'': {templateUrl: 'views/recruitment/createRequisition.html', controller: 'createRequisitionCtrl'}},
-	    	resolve : {
-	    		permission: function(authorizationService,$route) {
-	    			return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVAL"]);
-	            }
-	    	}
-	    })
-	    .state('recruitment.editRequisition', {url:'/editRequisition', views: {'': {templateUrl: 'views/recruitment/viewRequisition.html', controller: 'editRequisitionCtrl'}},
-	    	resolve : {
-	    		permission: function(authorizationService,$route) {
-	    			return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVAL"]);
-	            }
-	    	}
-	    })
-	    .state('recruitment.cloneRequisition', {url:'/cloneRequisition', views: {'': {templateUrl: 'views/recruitment/cloneRequisition.html', controller: 'cloneRequisitionCtrl'}},
-	    	resolve : {
-	    		permission: function(authorizationService,$route) {
-	    			return authorizationService.permissionCheck(["ROLE_ADMIN","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVAL"]);
-	            }
-	    	}
-	    })
+    })
+    .state('recruitment.createRequisition', {url:'/createRequisition', views: {'': {templateUrl: 'views/recruitment/createRequisition.html', controller: 'createRequisitionCtrl'}},
+    	resolve : {
+    		permission: function(authorizationService,$route) {
+    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_MANAGER","ROLE_ADMIN","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
+            }
+    	}
+    })
+    .state('recruitment.editRequisition', {url:'/editRequisition', views: {'': {templateUrl: 'views/recruitment/viewRequisition.html', controller: 'editRequisitionCtrl'}},
+    	resolve : {
+    		permission: function(authorizationService,$route) {
+    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_MANAGER","ROLE_ADMIN","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
+            }
+    	}
+    })
+    .state('recruitment.cloneRequisition', {url:'/cloneRequisition', views: {'': {templateUrl: 'views/recruitment/cloneRequisition.html', controller: 'cloneRequisitionCtrl'}},
+    	resolve : {
+    		permission: function(authorizationService,$route) {
+    			return authorizationService.permissionCheck(["ROLE_HR","ROLE_MANAGER","ROLE_ADMIN","ROLE_REQUISITION_MANAGER","ROLE_REQUISITION_APPROVER"]);
+            }
+    	}
+    })
 }]);
