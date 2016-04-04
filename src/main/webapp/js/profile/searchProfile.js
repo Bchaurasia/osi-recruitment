bb9b5daca5de4f076bc5b06959136a9accb442a1
rgund1 <rgund1@RGUND.nisum.com>
2016-04-04 20:47:41 +0530

    Added logos to footer and added search profile and SearchPosition pages

--- src/main/webapp/js/profile/searchProfile.js
+++ src/main/webapp/js/profile/searchProfile.js
@@ -1,11 +1,5 @@
 app.controller('searchProfileCtrl',['$scope', '$http','$q', '$window','jobCodeService1','$rootScope', '$filter', '$log','appConstants', function($scope, $http, $q, $window, jobCodeService1,$rootScope, $filter,$log,appConstants) {
-	
-	$scope.errorHide = true;
-	
-	$scope.col=["Name","Email Id","Designation","Experience","Current Employer","Assigned Job Code"];
-	
-	$scope.att=["candidateName","emailId","designation","expYear","currentEmployer","jobcodeProfile"];
-	$scope.att1=["jobcodeProfile"];
+
 	for (i = 0; i< $rootScope.user.roles.length;i++){
     	if($rootScope.user.roles[i] == 'ROLE_USER'){
     		var URLL = 'resources/profile?profilecreatedBy=' + $rootScope.user.emailId;
@@ -16,7 +10,7 @@ app.controller('searchProfileCtrl',['$scope', '$http','$q', '$window','jobCodeSe
 	$http.get(URLL).success(function(data, status, headers, config) {
 		$scope.myData = data;
 	}).error(function(data, status, headers, config) {
-		$log.error("Failed To Get Prfiles! ---> "+data);
+		$log.error("Failed To Get Profiles! ---> "+data);
 	});
 
 	$scope.title = "Search";
@@ -26,5 +20,55 @@ app.controller('searchProfileCtrl',['$scope', '$http','$q', '$window','jobCodeSe
 			jobCodeService1.setjobCode(data.jobcodeProfile);
 			location.href='#recruitment/viewProfile';
 		};
+		
+		$scope.itemsPerPage = appConstants.ITEMS_PER_PAGE;
+		$scope.currentPage = 0;
+		$scope.changePage = function(){
+			$scope.currentPage = 0;
+		}
+		
+		$scope.range = function (start) {
+			var pageCnt = $scope.pageCount();
+	        var ret = [];
+
+			if (start + 1 == pageCnt && pageCnt==1) {
+				ret.push(0);
+				return ret;
+			}
+			if ((start + 2 >= pageCnt)) {
+				while (start + 2 >= pageCnt)
+					start--;
+			}
+			if(start<0)
+				start=0;
+			for (var i = start; i < pageCnt; i++) {
+				ret.push(i);
+				if (i == start + 2)
+					break;
+			}
+			return ret;
+	    };
+
+		  $scope.prevPage = function() {
+		    if ($scope.currentPage > 0) {
+		      $scope.currentPage--;
+		    }
+		  };
+		
+		  $scope.pageCount = function() {
+			if (!$scope.myData) { return; }
+		    return Math.ceil($scope.myData.length/$scope.itemsPerPage);
+		  };
+		
+		  $scope.nextPage = function() {
+			  $scope.page = $scope.pageCount()-1;   
+		    if ($scope.currentPage < $scope.page) {
+		      $scope.currentPage++;
+		    }
+		  };
+		
+		  $scope.setPage = function() {
+		    $scope.currentPage = this.n;
+		  };
 		
 	}]);
