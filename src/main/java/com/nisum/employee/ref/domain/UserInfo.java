720b7344675e4fc469d6469678fd0981d710b214
shandesh madarpalle <shandesh_madarpalle@gap.com>
2016-04-05 14:30:38 +0530

    uodated userinfo collection as userInfo

--- src/main/java/com/nisum/employee/ref/domain/UserInfo.java
+++ src/main/java/com/nisum/employee/ref/domain/UserInfo.java
@@ -3,7 +3,6 @@ package com.nisum.employee.ref.domain;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Getter;
 import lombok.Setter;
