a17fa80f562d65fbf7f4a196b2ee539055e28cd8
shandesh madarpalle <shandesh_madarpalle@gap.com>
2016-04-14 20:22:55 +0530

    recruitment highlighting is resolved :) - ruturaj

--- src/main/webapp/js/recruitment/recruitment.js
+++ src/main/webapp/js/recruitment/recruitment.js
@@ -1,6 +1,9 @@
 app.controller("recruitmentCtrl", ['$scope', '$http', '$filter', '$timeout','$q','$state', '$location', 
     function($scope, $http, $filter, $timeout, $q, $state, $location) {
-
+	
+	if($state.is("recruitment"))
+    	$state.go("recruitment.searchProfile");
+	
 	$scope.isActive = function (stateName) {
 		$scope.indeX = $state.is(stateName);
         return $scope.indeX;
