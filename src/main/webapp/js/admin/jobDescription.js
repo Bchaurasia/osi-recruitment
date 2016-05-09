ac64aa154feb95bd1fd183d9c58dd5ab2e7ece60
osirecruitmentservice <123osi123>
2016-05-09 23:59:16 +0530

    [Bibha]- Fixed message showing issue in jobdescription page

--- src/main/webapp/js/admin/jobDescription.js
+++ src/main/webapp/js/admin/jobDescription.js
@@ -1,12 +1,15 @@
-app.controller('jobDescriptionListCtrl',['$scope','$rootScope', '$http','$q', '$window', '$timeout','$state','$filter','$log','appConstants','infoService','$location','$anchorScroll','jobDescriptionService','clientService','sharedService',
-                                      function($scope,$rootScope, $http, $q, $window, $timeout,$state,$filter,$log,appConstants,infoService,$location,$anchorScroll,jobDescriptionService,clientService,sharedService ) {
+app.controller('jobDescriptionListCtrl',['$scope','$rootScope', '$http','$q', '$window', '$timeout','$state','$filter','$log','appConstants','infoService','$location','$anchorScroll','jobDescriptionService','clientService','sharedService','sharedDataService',
+                                      function($scope,$rootScope, $http, $q, $window, $timeout,$state,$filter,$log,appConstants,infoService,$location,$anchorScroll,jobDescriptionService,clientService,sharedService,sharedDataService) {
 
 	$scope.col=["Job Title","Client","Skills"];
 	
 	$scope.att=["jobDescriptionName","client","skills"];
 	$scope.att1=["skills"];
+	$scope.cls = sharedDataService.getClass();
+	$scope.message = sharedDataService.getmessage();
 	
 	$scope.init = function() {
+		$timeout( function(){ $scope.message = ""; $scope.cls = ''; sharedDataService.setmessage("");sharedDataService.getClass("");}, 5000);
 		jobDescriptionService.getJobDescription().then(function(data){
 			$scope.jobDescriptionList=data;
 		}).catch(function(msg){
