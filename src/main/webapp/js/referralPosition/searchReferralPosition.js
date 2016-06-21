4817661b485ff17b8330b26a4d1824a90070056f
osirecruitmentservice <123osi123>
2016-06-21 20:35:43 +0530

    [Bibha]- Modified to view all new positions created to everyone under Referral  > POSITION tab.

--- src/main/webapp/js/referralPosition/searchReferralPosition.js
+++ src/main/webapp/js/referralPosition/searchReferralPosition.js
@@ -1,16 +1,95 @@
-app.controller("searchReferralPositionCtrl", ['$scope', '$http', '$filter', '$timeout','$q','$state', '$location','sharedService', 'positionService',
-    function($scope, $http, $filter, $timeout, $q, $state, $location, sharedService, positionService) {
+app.controller("searchReferralPositionCtrl", ['$scope', '$http', '$filter', '$timeout','$q','$state', '$location','sharedService', 'positionService','appConstants',
+    function($scope, $http, $filter, $timeout, $q, $state, $location, sharedService, positionService,appConstants) {
 	
-	var positionType="Referral";
-	positionService.getPositionsByPositionType(positionType).then(function(data){
+	//var positionType="Referral";
+	$scope.searchQuery="";
+	
+	/*positionService.getPositionsByPositionType(positionType).then(function(data){
 		$scope.allReferralPositions = data;
 	}).catch(function(msg){
     	$log.error(msg); 
-    });
+    });*/
 	
-	$scope.displayReferralPosition = function(jobcodePosition) {
-		sharedService.setjobCode(jobcodePosition);
-		location.href='#referral/viewReferralPosition';
+	$scope.searchPositionQuery = function(){
+		if(sharedService.getDesignation() != undefined && sharedService.getDesignation() != null) {
+			$scope.searchQuery=sharedService.getDesignation();
+			sharedService.setDesignation(null);
+		}	
+		positionService.searchPositionsBySearchQuery($scope.searchQuery).then(function(data){
+			$scope.positions = data;
+				$scope.currentPage = 0;
+				$scope.searchQuery="";
+		}).catch(function(msg){
+	   	  $log.error("Failed To Load Data! ---> "+msg);
+	     });
+	}
+	
+	$scope.searchPositionQuery(); //getting all data first time
+	$scope.itemsPerPage = appConstants.ITEMS_PER_PAGE;
+	$scope.currentPage = 0;
+	$scope.changePage = function(){
+		$scope.currentPage = 0;
+	}
+	
+	$scope.sortComment = function(comment) {
+	    var date = new Date(comment.created);
+	    return date;
 	};
-	    
+	$scope.range = function (start) {
+		var pageCnt = $scope.pageCount();
+        var ret = [];
+
+		if (start + 1 == pageCnt && pageCnt==1) {
+			ret.push(0);
+			return ret;
+		}
+		if ((start + 2 >= pageCnt)) {
+			while (start + 2 >= pageCnt)
+				start--;
+		}
+		if(start<0)
+			start=0;
+		for (var i = start; i < pageCnt; i++) {
+			ret.push(i);
+			if (i == start + 2)
+				break;
+		}
+		return ret;
+    };
+
+		  $scope.prevPage = function() {
+		    if ($scope.currentPage > 0) {
+		      $scope.currentPage--;
+		    }
+		  };
+
+		  $scope.pageCount = function() {
+			if (!$scope.positions) { return; }
+		    return Math.ceil($scope.positions.length/$scope.itemsPerPage);
+		  };
+
+		  $scope.nextPage = function() {
+			  $scope.page = $scope.pageCount()-1;   
+		    if ($scope.currentPage < $scope.page) {
+		      $scope.currentPage++;
+		    }
+		  };
+
+		  $scope.setPage = function() {
+		    $scope.currentPage = this.n;
+		  };
+	
+	
+		  $scope.displayReferralPosition = function(jobcodePosition) {
+				sharedService.setjobCode(jobcodePosition);
+				location.href='#referral/viewReferralPosition';
+			};
+			    
 }]);
+
+app.filter('offset', function() {
+	  return function(input, start) {
+	  start = parseInt(start, 10);
+return input.slice(start);
+};
+});
