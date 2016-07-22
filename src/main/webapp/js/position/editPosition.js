b69a4482770e6fbe1a9dcbdbfb011a49ecc78104
arajak <arajak@MININT-K3KRGQ7>
2016-07-22 16:22:33 +0530

    #added 'joined' status in position

--- src/main/webapp/js/position/editPosition.js
+++ src/main/webapp/js/position/editPosition.js
@@ -23,7 +23,7 @@ app.controller("editPositionCtrl",   ['$scope','$state', '$http','sharedService'
 	$scope.pskills = [];
 	$scope.message = "";
 	$scope.client =[];
-	$scope.statuses = ["Active", "Inactive", "Hired","OnHold","Selected","Rejected","Closed"];
+	$scope.statuses = ["Active", "Inactive", "Hired","OnHold","Selected","Rejected","Joined","Closed"];
 	$scope.priorities=["Low","Medium","High"];
 	$scope.hideStatuses = true;
 	$scope.userRole = $rootScope.user.roles;
