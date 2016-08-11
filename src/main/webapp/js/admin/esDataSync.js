009f6b19495c0c260830dd1a925fec692f9d026e
bchaurasia <bchaurasia@BCHAURASIA.nisum.com>
2016-08-11 20:37:34 +0530

    Fixed EsDataSync issue in Admin page.

--- src/main/webapp/js/admin/esDataSync.js
+++ src/main/webapp/js/admin/esDataSync.js
@@ -1,4 +1,4 @@
-app.controller('esDataSyncCtrl',['$scope','$rootScope', '$log','$http','$q',function($scope,$rootScope,$log, $http,$q){
+app.controller('esDataSyncCtrl',['$scope','$rootScope', '$log','$http','$q','$timeout',function($scope,$rootScope,$log, $http,$q,$timeout){
 	
 	$scope.deleteIndex = function(){
 		$http.post( 'resources/deleteDataIndex').
@@ -6,7 +6,7 @@ app.controller('esDataSyncCtrl',['$scope','$rootScope', '$log','$http','$q',func
 			  $scope.cls = 'alert  alert-success';
 			  $scope.message = 'Data index deleted successfully';
 			  $log.info("index deleted Successfully!"+data);
-			  $timeout( function(){ $scope.alHide(); }, 5000);
+			  $timeout( function(){ $scope.alHide(); }, 3000);
 			  $scope.reset();
 		  }).
 		  error(function(data) {
@@ -20,7 +20,7 @@ app.controller('esDataSyncCtrl',['$scope','$rootScope', '$log','$http','$q',func
 			  $scope.cls = 'alert  alert-success';
 			  $scope.message = 'Data index updated successfully';
 			  $log.info("index updated Successfully!"+data);
-			  $timeout( function(){ $scope.alHide(); }, 5000);
+			  $timeout( function(){ $scope.alHide(); }, 3000);
 			  $scope.reset();
 		  }).
 		  error(function(data) {
@@ -34,5 +34,12 @@ app.controller('esDataSyncCtrl',['$scope','$rootScope', '$log','$http','$q',func
 	
 	function createPositionError(response){
 		return "Failed To delete " + response.data;
+	}
+	
+	$scope.alHide = function(){
+		  	$scope.cls = '';
+		  $scope.message = '';
+		  sharedDataService.setmessage("");
+		  sharedDataService.getClass("");
 	}
 }]);
