913af5775568fbf7b5060c9140be1e793d10578d
Sanket Nehe <Sanket Nehe>
2016-09-09 15:15:09 +0530

    Image slider

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -669,6 +669,62 @@ $scope.showInterview = function(obj, obj2) {
 };
 
 
+jQuery(document).ready(function ($) {
+    var options = {
+        $AutoPlay: true,           
+		$Idle: 2000,     
+		$PauseOnHover: 1,     
+		
+        $ArrowKeyNavigation: true, 
+		$SlideEasing: $JssorEasing$.$EaseOutQuint,  
+		$SlideDuration: 4000,   
+		$MinDragOffsetToSlide: 10,     
+		$SlideSpacing: 0, 	
+		$Cols: 1,         
+		$ParkingPosition: 0,      
+		$UISearchMode: 1,   
+		$PlayOrientation: 1,              
+		$DragOrientation: 3, 
+		
+        $ArrowNavigatorOptions: {   
+		$Class: $JssorArrowNavigator$,         
+		$ChanceToShow: 2,                   
+		$AutoCenter: 2,
+		$Steps: 1,   
+		$Scale: false                  
+		},
+
+        $BulletNavigatorOptions: {          
+		$Class: $JssorBulletNavigator$,              
+		$ChanceToShow: 2,           
+		$AutoCenter: 1,               
+		$Steps: 1,       
+		$Rows: 1,     
+		$SpacingX: 12, 
+		$SpacingY: 4,   
+		$Orientation: 1,     
+		$Scale: false    
+		}
+    };
+
+    var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+
+    function ScaleSlider() {
+        var parentWidth = jssor_slider1.$Elmt.parentNode.clientWidth;
+        if (parentWidth) {
+            jssor_slider1.$ScaleWidth(parentWidth - 30);
+        }
+        else
+            window.setTimeout(ScaleSlider, 30);
+    }
+    ScaleSlider();
+
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    //responsive code end
+});
+
 
 
 }]);
